031e66116a79ae797abb081d5baf4fa67445abcf
Rajithkonara <rajithk@wso2.com>
2019-12-11 11:15:32 +0530

    first problem

---
+++ src/test/java/com/singtel/one/BirdTest.java
@@ -0,0 +1,37 @@
+package com.singtel.one;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BirdTest {
+
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final  PrintStream originalOut = System.out;
+
+
+    @BeforeAll
+    public static void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restore() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testBirdCanSing() {
+
+        new Bird().sing();
+        assertEquals(outContent.toString(), "I am singing\n");
+
+    }
+
+}
